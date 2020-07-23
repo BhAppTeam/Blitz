a487c720a9da08ca50bbd0e23b3df949bf16c187
kaspersoreide <kasper.soreide@gmail.com>
2020-07-23 20:24:41 +0200

    now with svelte template and routify!

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,10 @@
 <script>
-    import Profile from './Profile.svelte';
-
-    import { auth, googleProvider } from './firebase';
-	import { authState } from 'rxfire/auth';
-
-    let user;
-
-    const unsubscribe = authState(auth).subscribe(u => user = u);
-
-    function login() {
-        auth.signInWithPopup(googleProvider);
-    }
+  import { Router } from "@sveltech/routify";
+  import { routes } from "../.routify/routes";
 </script>
 
+<style  global>
+  @import "../static/global.css";
+</style>
 
-<section>
-{#if user}
-    <Profile {...user} />
-    <button on:click={ () => auth.signOut() }>Logout</button>
-    <hr>
-{:else}
-	<button on:click={login}>
-		Signin with Google
-	</button>
-{/if}
-</section>
+<Router {routes} />
